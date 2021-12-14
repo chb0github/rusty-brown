b576a05307adb7dafd17e041ac6a56b77f6f63ec
christian <christianb@jfrog.com>
2021-12-14 15:25:41 -0700

    It works. Time to make it better and demo it

--- src/block_chain.rs
+++ src/block_chain.rs
@@ -1,11 +1,18 @@
-use std::borrow::Borrow;
 use chrono::Utc;
-use log::{error, info, warn};
+use log::{ info, warn};
 use sha2::{Digest, Sha256};
-
+use serde::{Deserialize, Serialize};
 
 pub struct BlockChain {
     blocks: Vec<Block>,
+}
+
+impl BlockChain {
+    pub(crate) fn dump(&self) {
+        serde_json::to_string_pretty(&self.blocks).map(|pretty_json|
+            println!("{}", pretty_json)
+        );
+    }
 }
 
 const DIFFICULTY_PREFIX: &str = "00";
@@ -13,74 +20,68 @@ const DIFFICULTY_PREFIX: &str = "00";
 impl BlockChain {
     pub fn new() -> Self {
         BlockChain {
-            blocks: Vec::from([BlockChain::genesis()])
+            blocks: vec![]
         }
     }
-    fn genesis() -> Block {
-        Block {
+    pub fn genesis(&mut self) {
+        let genesis_block = Block {
             id: 0,
             timestamp: Utc::now().timestamp(),
             previous_hash: String::from("genesis"),
             data: String::from("genesis!"),
-            nonce: 1,
-            hash: String::from("0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43"),
-        }
+            nonce: 2836,
+            hash: "0000f816a87f806bb0073dcf026a64fb40c946b5abee2573702828694d5b4c43".to_string(),
+        };
+        self.blocks.push(genesis_block);
     }
 
+
     pub fn add_block(&mut self, data: String) -> &Block {
-        let last = self.blocks.last().expect("always a last block");
-        let block = self.mk_block(last.id.clone(), last.hash.clone(), data).expect("error creating block");
+        let block = self.mk_block( data.clone()).expect("error creating block");
         self.blocks.push(block);
         self.blocks.last().expect("has a block")
     }
-    fn mk_block(&mut self, id: u64, last_hash: String, data: String) -> Option<Block> {
-        let block = Block::new(id + 1,last_hash,data);
-        let latest_block = self.blocks.last().expect("there is at least one block");
-        if self.is_block_valid(&block, latest_block) {
-            return Some(block)
+    fn mk_block(&mut self, data: String) -> Option<Block> {
+        let last_block = self.blocks.last().expect("there is at least one block");
+        let new_block = Block::new(last_block.id.clone() + 1, last_block.hash.clone(), data);
+        if self.is_block_valid(&new_block, last_block) {
+            return Some(new_block)
         }
         None
 
     }
-
 
     fn is_block_valid(&self, block: &Block, previous_block: &Block) -> bool {
         if block.previous_hash != previous_block.hash {
             warn!("block with id: {} has wrong previous hash", block.id);
             return false;
         }
-        let hash = &hex::decode(&block.hash).expect("can decode from hex");
-        let invalid_difficulty = !hash_to_binary_representation(hash).starts_with(DIFFICULTY_PREFIX);
-        if invalid_difficulty {
-            warn!("block with id: {} has invalid difficult", block.id);
+        if !hash_to_binary_representation(&hex::decode(&block.hash).expect("can decode from hex")).starts_with(DIFFICULTY_PREFIX)
+        {
+            warn!("block with id: {} has invalid difficulty", block.id);
             return false;
         }
         if block.id != previous_block.id + 1 {
-            warn!("block id: {} is not the next block after the latest: {}",block.id,previous_block.id);
+            warn!(
+                "block with id: {} is not the next block after the latest: {}",
+                block.id, previous_block.id
+            );
             return false;
         }
-        let block_hash = hex::encode(
-            BlockChain::calculate_hash(block.id, block.timestamp, &block.previous_hash, &block.data, block.nonce)
-        );
-        if block_hash != block.hash {
+        if hex::encode(calculate_hash(
+            block.id,
+            block.timestamp,
+            &block.previous_hash,
+            &block.data,
+            block.nonce,
+        )) != block.hash
+        {
             warn!("block with id: {} has invalid hash", block.id);
             return false;
         }
         true
     }
 
-    fn calculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
-        let data = serde_json::json!({
-            "id": id,
-            "previous_hash": previous_hash,
-            "data": data,
-            "timestamp": timestamp,
-            "nonce": nonce
-        });
-        let mut hasher = Sha256::new();
-        hasher.update(data.to_string().as_bytes());
-        hasher.finalize().as_slice().to_owned()
-    }
     fn is_chain_valid(&self, chain: &[Block]) -> bool {
         for i in 1..chain.len() {
             let first = chain.get(i - 1).expect("has to exist");
@@ -91,25 +92,9 @@ impl BlockChain {
         }
         true
     }
-
-    fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
-        info!("mining block...");
-
-        for nonce in 0..u64::MAX{
-            let hash = BlockChain::calculate_hash(id, timestamp, previous_hash, data, nonce);
-            let binary_hash = hash_to_binary_representation(&hash);
-            if binary_hash.starts_with(DIFFICULTY_PREFIX) {
-                return (nonce, hex::encode(hash));
-            }
-        }
-        panic!("unable to to mine block")
-    }
-}
-fn hash_to_binary_representation(hash: &[u8]) -> String {
-    // editor says join is a thing. Compiler says otherwise
-    hash.iter().map(|c| format!("{:b}", c)).fold("".to_string(), |cur, nxt| cur + &nxt)
 }
 
+#[derive(Serialize, Deserialize, Debug, Clone)]
 pub struct Block {
     pub id: u64,
     pub hash: String,
@@ -119,19 +104,43 @@ pub struct Block {
     pub nonce: u64,
 }
 
-
 impl Block {
     pub fn new(id: u64, previous_hash: String, data: String) -> Self {
-        let (nonce,hash) = BlockChain::mine_block(id, Utc::now().timestamp(), &previous_hash, &data);
-        Block{
+        let now = Utc::now();
+        let (nonce, hash) = mine_block(id, now.timestamp(), &previous_hash, &data);
+        Self {
             id,
             hash,
+            timestamp: now.timestamp(),
             previous_hash,
-            timestamp: Utc::now().timestamp(),
             data,
             nonce,
         }
     }
+}
+
+fn mine_block(id: u64, timestamp: i64, previous_hash: &str, data: &str) -> (u64, String) {
+    info!("mining block...");
+
+    (0..u64::MAX).map(|nonce| (nonce, calculate_hash(id, timestamp, previous_hash, data, nonce)))
+        .map(|(nonce,hash) | (nonce,hash.clone(),hash_to_binary_representation(&hash.clone())))
+        .find(|(nonce,hash,binary_hash)| binary_hash.starts_with(DIFFICULTY_PREFIX))
+        .map(|(nonce,hash, bin)| (nonce, hex::encode(hash))).expect("results")
+}
+fn calculate_hash(id: u64, timestamp: i64, previous_hash: &str, data: &str, nonce: u64) -> Vec<u8> {
+    let data = serde_json::json!({
+        "id": id,
+        "previous_hash": previous_hash,
+        "data": data,
+        "timestamp": timestamp,
+        "nonce": nonce
+    });
+    let mut hasher = Sha256::new();
+    hasher.update(data.to_string().as_bytes());
+    hasher.finalize().as_slice().to_owned()
+}
+fn hash_to_binary_representation(hash: &[u8]) -> String {
+    hash.iter().map(|c| format!("{:b}", c)).fold("".to_string(), |cur, nxt| cur + &nxt)
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::io::{BufRead, BufReader, stdin};
+use std::process::exit;
 use crate::block_chain::BlockChain;
 
 
@@ -7,8 +8,16 @@ mod block_chain;
 
 fn main() {
     let mut bc = BlockChain::new();
+    bc.genesis();
 
     BufReader::new(stdin()).lines()
         .map(|l| l.unwrap())
-        .take_while(|l| l != "quit").for_each(|l| { bc.add_block(l); })
+        .take_while(|l| l != "quit").for_each(|l|
+            match l.as_str() {
+                "quit" => exit(0),
+                "dump" => bc.dump(),
+                _ => { bc.add_block(l); }
+            }
+        )
 }
+
